9da7925d41b1dabde1fb40bdd4806c938930f7b5
AEJester <aftereffectsjester@gmail.com>
2018-02-03 16:52:43 -0500

    Added TEXT variable item.

--- src/variables/variableitems.js
+++ src/variables/variableitems.js
@@ -1,10 +1,33 @@
 exports.NUMBER = class NUMBER {
     constructor(value) {
+        if (!typeof value == "number") {
+            throw new Error("Value of a NUMBER must not be a "+ typeof value)
+        }
         this.value = value;
         this.returned = "";
     }
     get() {
         this.returned = this.value.toString();
+        return this.returned;
+    }
+    set(value) {
+        if (!typeof value == "string") {
+            throw new Error("Value of a TEXT must not be a "+ typeof value)
+        }
+        this.value = value;
+        return this;
+    }
+}
+exports.TEXT = class TEXT {
+    constructor(value) {
+        if (!typeof value == "string") {
+            throw new Error("Value of a TEXT must not be a "+ typeof value)
+        }
+        this.value = value;
+        this.returned = "";
+    }
+    get() {
+        this.returned = this.value;
         return this.returned;
     }
     set(value) {
