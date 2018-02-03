a02b7900f5ebd7158ac50189177d67d43c6eeb1c
AEJester <aftereffectsjester@gmail.com>
2018-02-03 17:00:16 -0500

    Added LOCATION variable item.

--- src/variables/variableitems.js
+++ src/variables/variableitems.js
@@ -35,3 +35,52 @@ exports.TEXT = class TEXT {
         return this;
     }
 }
+exports.LOCATION = class LOCATION {
+    constructor(x, y, z, rotation) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rot = rot;
+    }
+    get() {
+        return {x: this.x, y: this.y, z: this.z, rot: this.rot};
+    }
+    set(x, y, z, rotation) {
+        if (!typeof x == "number" || !typeof y == "number" || !typeof z == "number" || !typeof rot == "number") {
+            throw new Error("Value of an axis must not be a "+ typeof value)
+        }
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rot = rot;
+        return this;
+    }
+    setX(x) {
+        if (!typeof x == "number") {
+            throw new Error("Value of an axis must not be a "+ typeof value)
+        }
+        this.x = x;
+        return this;
+    }
+    setY(y) {
+        if (!typeof y == "number") {
+            throw new Error("Value of an axis must not be a "+ typeof value)
+        }
+        this.y = y;
+        return this;
+    }
+    setZ(z) {
+        if (!typeof z == "number") {
+            throw new Error("Value of an axis must not be a "+ typeof value)
+        }
+        this.z = z;
+        return this;
+    }
+    setRot(rot) {
+        if (!typeof rot == "number") {
+            throw new Error("Value of an axis must not be a "+ typeof value)
+        }
+        this.rot = rot;
+        return this;
+    }
+}
