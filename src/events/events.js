8fcc2667155c916122d96cba2d4b249467e49c92
Skezza4Expert <36114463+Skezza4Expert@users.noreply.github.com>
2018-02-03 20:35:04 +0000

    Update events.js

--- src/events/events.js
+++ src/events/events.js
@@ -19,7 +19,7 @@ exports.event = class Event {
     isEvent(event) {
         for (var i = 0; i < this.events.length; i++) {
             if (!event == this.events[i]) {
-                throw new Error("The event specified is not a real event. Please use a different event.")
+                throw new Error("Event not recognised! Please use a different event.")
             }
         }
         return this;
