0fc825e172678e54490d318c7de11e3ca4af2f8f
AEJester <aftereffectsjester@gmail.com>
2018-02-03 21:11:08 -0500

    Removed events.js - migrated to blocks/playerevent.js

---
+++ src/blocks/playerevent.js
@@ -0,0 +1,45 @@
+module.exports = class PlayerEvent {
+    constructor() {
+        this.events = [
+            "PlayerJoin",
+            "PlayerLeave",
+            "PlayerRightClick",
+            "PlayerLeftClick",
+            "PlayerRespawn",
+            "PlayerDeath",
+            "PlayerSneak",
+            "PlayerUnsneak",
+            "PlayerDamagePlayer",     
+            "PlayerProjDamagePlayer", 
+            "PlayerTakeDamage",
+            "PlayerKillPlayer",
+            "PlayerKillMob",
+            "MobKillPlayer",
+            "PlayerDamageMob",
+            "MobDamagePlayer",
+            "ProjectileHit",
+            "PlayerCommand",
+            "PlayerClickItem",
+            "PlayerClickEntity",
+            "PlayerPlaceBlock",
+            "PlayerBreakBlock",
+            "PlayerPickupItem",
+            "PlayerDropItem",
+            "PlayerConsume",
+            "PlayerSwapHands",
+            "PlayerChangeSlot",
+            "PlayerStartSprint",
+            "PlayerStopSprint",
+            "PlayerWalk",
+            "PlayerTakeFallDamage"
+        ];
+    }
+    isEvent(event) {
+        for (var i = 0; i < this.events.length; i++) {
+            if (event !== this.events[i]) {
+                throw new Error("Event not recognised! Please use a different event.")
+            }
+        }
+        return this;
+    }
+}
