6e68fc3fc7947f4652b019a8d377f5385d415478
Skezza4Expert <36114463+Skezza4Expert@users.noreply.github.com>
2018-02-12 19:41:09 +0000

    Update gameactions.js
    Added in all actions

---
+++ src/actions/gameactions.js
@@ -0,0 +1,57 @@
+ 
+ spawnMob(spawn_egg, location, number, text, potion_effects, items) {
+ 
+ spawnItem(item, location, text) {
+ 
+ wait(number) {
+ 
+ createExplosion(location, number) {
+ 
+ SpawnTnt(location, number, number) {
+ 
+ launchFireworks(FIREWORK, location, text) {
+ 
+ fireworkExplosion(FIREWORK, location) {
+ 
+ fillHolder(location, items) {
+ 
+ emptyHolder(location) {
+ 
+ startLoop() {
+ 
+ stopLoop() {
+ 
+ setBlock(block, location, OPTIONAL LOCATION 2) {
+ 
+ breakBlock(location) {
+ 
+ copyBlocks(location, location, location, location) {
+ 
+ createHologram(location, text) {
+ 
+ removeHologram(LOCATION OR TEXT) {
+ 
+ showScoreboard() {
+ 
+ hideScoreboard() {
+ 
+ setObjective(text) {
+ 
+ setScore(text, number) {
+ 
+ removeScore(text) {
+ 
+ clearScoreboard() {
+ 
+ enableBlockDrops() {
+ 
+ disableBlockDrops() {
+ 
+ cancelEvent() {
+ 
+ spawnXpOrb(location, number, text) {
+ 
+ spawnVehicle(VEHICLE, location, text, number) {
+ 
+ changeSign(location, number, text) {
+ 
