e048e65f3c4b4e80a75c084415126b95d664bb95
AEJester <aftereffectsjester@gmail.com>
2018-03-28 17:15:13 -0400

    Made a GameAction class, took Skezza's work and JavaScript-ified it.

--- src/actions/gameactions.js
+++ src/actions/gameactions.js
@@ -1,57 +1,119 @@
  
- spawnMob(spawn_egg, location, number, text, potion_effects, items) {
+ module.exports = class GameAction {
+     constructor() {
+
+     }
+     spawnMob(spawn_egg, location, number, text, potion_effects, items) {
+
+     }
  
- spawnItem(item, location, text) {
+      spawnItem(item, location, text) {
+
+      }
+        
+      wait(number) {
+
+      }
+        
+      createExplosion(location, number) {
+
+      }
+        
+      SpawnTnt(location, number, number) {
+
+      }
+        
+      launchFireworks(FIREWORK, location, text) {
+
+      }
+        
+      fireworkExplosion(FIREWORK, location) {
+
+      }
+        
+      fillHolder(location, items) {
+
+      }
+        
+      emptyHolder(location) {
+
+      }
+        
+      startLoop() {
+
+      }
+        
+      stopLoop() {
+
+      }
+        
+      setBlock(block, location, opts={}) {
+
+      }
+        
+      breakBlock(location) {
+
+      }
+        
+      copyBlocks(location, location, location, location) {
+
+      }
+        
+      createHologram(location, text) {
+
+      }
+        
+      removeHologram(opts={}) {
+
+      }
+        
+      showScoreboard() {
+
+      }
+        
+      hideScoreboard() {
+
+      }
+        
+      setObjective(text) {
+
+      }
+        
+      setScore(text, number) {
+
+      }
+        
+      removeScore(text) {
+
+      }
+        
+      clearScoreboard() {
+
+      }
+        
+      enableBlockDrops() {
+
+      }
+        
+      disableBlockDrops() {
+
+      }
+        
+      cancelEvent() {
+
+      }
+        
+      spawnXpOrb(location, number, text) {
+
+      }
+        
+      spawnVehicle(VEHICLE, location, text, number) {
+
+      }
+        
+      changeSign(location, number, text) {
+          
+      }
  
- wait(number) {
- 
- createExplosion(location, number) {
- 
- SpawnTnt(location, number, number) {
- 
- launchFireworks(FIREWORK, location, text) {
- 
- fireworkExplosion(FIREWORK, location) {
- 
- fillHolder(location, items) {
- 
- emptyHolder(location) {
- 
- startLoop() {
- 
- stopLoop() {
- 
- setBlock(block, location, OPTIONAL LOCATION 2) {
- 
- breakBlock(location) {
- 
- copyBlocks(location, location, location, location) {
- 
- createHologram(location, text) {
- 
- removeHologram(LOCATION OR TEXT) {
- 
- showScoreboard() {
- 
- hideScoreboard() {
- 
- setObjective(text) {
- 
- setScore(text, number) {
- 
- removeScore(text) {
- 
- clearScoreboard() {
- 
- enableBlockDrops() {
- 
- disableBlockDrops() {
- 
- cancelEvent() {
- 
- spawnXpOrb(location, number, text) {
- 
- spawnVehicle(VEHICLE, location, text, number) {
- 
- changeSign(location, number, text) {
- 
+
+ }
